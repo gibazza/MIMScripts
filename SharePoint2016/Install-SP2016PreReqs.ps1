3636fa4c29c8a6136c055a512fa8852d79593494
Paul Barratt <Paul@boyar.org.uk>
2019-01-31 16:54:49 +0000

    Fixed Prereq install arguments

--- SharePoint2016/Install-SP2016PreReqs.ps1
+++ SharePoint2016/Install-SP2016PreReqs.ps1
@@ -27,7 +27,7 @@ Move-Item -Path "$($SharePoint2016Path)Microsoft Sync Framework\Synchronization.
 if($?)
 {
     Remove-Item -Force -Recurse -Path "$($SharePoint2016Path)Microsoft Sync Framework"
-    Remove-Item -Force -Path "$($SharePoint2016Path)SyncSetup_en.x64.zip"
+    Remove-Item -Force -Path "$($SharePoint2016Path)SyncSetup_en.x64.zip"-
 }
 
 Remove-Item -Force -Recurse -Path "$($SharePoint2016Path)DotNetFx"
@@ -35,4 +35,4 @@ Remove-Item -Force -Recurse -Path "$($SharePoint2016Path)Microsoft Sync Framewor
 Remove-Item -Force -Recurse -Path "$($SharePoint2016Path)Microsoft Sync Framework Services"
 Remove-Item -Force -Recurse -Path "$($SharePoint2016Path)Microsoft Sync Services for ADO"
 
-Start-Process "$($SharePoint2016InstallPath)PrerequisiteInstaller.exe" –ArgumentList "/SQLNCli:($SharePoint2016Path)sqlncli.msi /IDFX11:($SharePoint2016Path)MicrosoftIdentityExtensions-64.msi /Sync:($SharePoint2016Path)Synchronization.msi /AppFabric:($SharePoint2016Path)WindowsServerAppFabricSetup_x64.exe /MSIPCClient:($SharePoint2016Path)setup_msipc_x64.exe /WCFDataServices56:($SharePoint2016Path)WcfDataServices.exe /DotNetFx:($SharePoint2016Path)NDP453-KB2969351-x86-x64-AllOS-ENU.exe /MSVCRT11:($SharePoint2016Path)vcredist_x64.exe /MSVCRT14:($SharePoint2016Path)vc_redist.x64.exe /KB3092423:($SharePoint2016Path)AppFabric-KB3092423-x64-ENU.exe"
+Start-Process "$($SharePoint2016InstallPath)PrerequisiteInstaller.exe" -ArgumentList "/SQLNCli:$($SharePoint2016Path)sqlncli.msi /IDFX11:$($SharePoint2016Path)MicrosoftIdentityExtensions-64.msi /Sync:$($SharePoint2016Path)Synchronization.msi /AppFabric:$($SharePoint2016Path)WindowsServerAppFabricSetup_x64.exe /MSIPCClient:$($SharePoint2016Path)setup_msipc_x64.exe /WCFDataServices56:$($SharePoint2016Path)WcfDataServices.exe /DotNetFx:$($SharePoint2016Path)NDP453-KB2969351-x86-x64-AllOS-ENU.exe /MSVCRT11:$($SharePoint2016Path)vcredist_x64.exe /MSVCRT14:$($SharePoint2016Path)vc_redist.x64.exe /KB3092423:$($SharePoint2016Path)AppFabric-KB3092423-x64-ENU.exe"
