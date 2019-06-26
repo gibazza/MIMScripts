f0bfbe3735e97178059c87e63ac848abedf7f9a3
Paul Barratt <Paul@boyar.org.uk>
2019-06-26 10:17:57 +0100

    Add get local time script

---
+++ MIM2016/MIMWal/Get-LocalTime.ps1
@@ -0,0 +1,9 @@
+param
+(
+    [parameter(Mandatory=$true)] [String] $UTCTime
+)
+
+$strCurrentTimeZone = (Get-WmiObject win32_timezone).StandardName
+$TZ = [System.TimeZoneInfo]::FindSystemTimeZoneById($strCurrentTimeZone)
+$LocalTime = [System.TimeZoneInfo]::ConvertTimeFromUtc($UTCTime, $TZ) | Out-String
+Return $LocalTime
